b0bffe3ee12bf35508241eea05d40faec3166cba
manojaccellor <manoj.karumanchi@accellor.com>
2025-08-04 20:38:28 +0530

    Fix GitHub Pages deployment for cursor-test branch

--- sw.js
+++ sw.js
@@ -4,7 +4,6 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/styles.css',
-  '/config.js',
   '/script.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
